8411a83f3e9532a75a9e764e30ce0c8a8a9ea855
Your Name <you@example.com>
2023-11-19 13:18:27 +0800

    versionTwo

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,6 @@ import Header from './components/Header.vue'
   <div class="header">
     <Header />
   </div>
-
-  <!-- <div class="quan">
-    <div class="shui"></div>
-  </div> -->
-  
   <RouterView />
 </template>
 
@@ -21,70 +16,5 @@ import Header from './components/Header.vue'
     width: 100vw;
     height: 23vh;
   }
-//會動的水
-  // .quan {
-  //     position: relative;
-  //     width: 180px;
-  //     height: 180px;
-  //     border-radius: 50%;
-  //     border: 3px solid rgb(246, 247, 248);
-  //     box-shadow: 0 0 0 3px rgb(41, 134, 196);
-  //   }
-
-  //   .shui {
-  //     position: absolute;
-  //     top: 0;
-  //     left: 0;
-  //     width: 100%;
-  //     height: 100%;
-  //     background-color: lightskyblue;
-  //     border-radius: 50%;
-  //     overflow: hidden;
-  //   }
-
-  //   .shui::after {
-  //     content: '';
-  //     position: absolute;
-  //     top: 0;
-  //     left: 50%;
-  //     width: 150%;
-  //     height: 150%;
-  //     border-radius: 40%;
-  //     background-color: lightblue;
-  //     animation: shi 5s linear infinite;
-  //   }
-
-  //   @keyframes shi {
-  //     0% {
-  //       transform: translate(-50%, -65%) rotate(0deg);
-  //     }
-
-  //     100% {
-  //       transform: translate(-50%, -65%) rotate(360deg);
-  //     }
-  //   }
-
-  //   .shui::before {
-  //     content: '';
-  //     position: absolute;
-  //     top: 0;
-  //     left: 50%;
-  //     width: 150%;
-  //     height: 150%;
-  //     border-radius: 42%;
-  //     background-color: rgb(240, 228, 228, 0.2);
-  //     animation: xu 7s linear infinite;
-  //   }
-
-  //   @keyframes xu {
-  //     0% {
-  //       transform: translate(-50%, -60%) rotate(0deg);
-  //     }
-
-  //     100% {
-  //       transform: translate(-50%, -60%) rotate(360deg);
-  //     }
-  //   }
-
 
 </style>
--- src/views/Visual.vue
+++ src/views/Visual.vue
@@ -2,12 +2,6 @@
     export default{
         data(){
             return{
-            //北部
-            arrN:[1,2,3,4,5,6],
-            //中部
-            arrM:[1,2,3,4,5,6,7,8,9],
-            //南部
-            arrS:[1,2,3,4,5,6,7],
             }
         }
     }
@@ -19,37 +13,109 @@
         <input type="text" placeholder="開始搜尋">
         <button type="button">搜尋</button>
     </div>
-<!-- 北區 -->
     <div class="content">
 
+<!-- 北區 -->
+    <label for="">北區</label>
         <div class="north">
-            <label for="">北區</label>
-            <!-- <div  v-for="item in arrN" class="northItem">
-            </div> -->
             <!-- 石門水庫 -->
             <div class="northOne">
                 <div class="nameOne">
                     <h2>石門水庫</h2>
                 </div>
             <!-- 本日累積雨量，Inflow Total -->
-                <p id="IFTNO"></p>
+                <p id="IFTNO">本日累積雨量</p>
             <!-- 昨日累積降雨量，Basin Rain Fall -->
-                <p id="BRFNO"></p>
+                <p id="BRFNO">昨日累積降雨量</p>
             <!-- 有效蓄水量，Effective Storage -->
-                <p id="ESNO"></p>
+                <p id="ESNO">有效蓄水量</p>
             <!-- 有效庫容量，Effective Capacity -->
-                <p id="ECNO"></p>
+                <p id="ECNO">有效庫容量</p>
             </div>
+
+            <!-- 西勢水庫 -->
+            <div class="northTwo">
+                <div class="nameTwo">
+                    <h2>西勢水庫</h2>
+                </div>
+            <!-- 本日累積雨量，Inflow Total -->
+                <p id="IFTNT"></p>
+            <!-- 昨日累積降雨量，Basin Rain Fall -->
+                <p id="BRFNT"></p>
+            <!-- 有效蓄水量，Effective Storage -->
+                <p id="ESNT"></p>
+            <!-- 有效庫容量，Effective Capacity -->
+                <p id="ECNT"></p>
+            </div>
+
+            <!-- 新山水庫 -->
+            <div class="northThree">
+                <div class="nameThree">
+                    <h2>新山水庫</h2>
+                </div>
+            <!-- 本日累積雨量，Inflow Total -->
+                <p id="IFTNTH"></p>
+            <!-- 昨日累積降雨量，Basin Rain Fall -->
+                <p id="BRFNTH"></p>
+            <!-- 有效蓄水量，Effective Storage -->
+                <p id="ESNTH"></p>
+            <!-- 有效庫容量，Effective Capacity -->
+                <p id="ECNTH"></p>
+            </div>
+
+            <!-- 翡翠水庫 -->
+            <div class="northFour">
+                <div class="nameFour">
+                    <h2>翡翠水庫</h2>
+                </div>
+            <!-- 本日累積雨量，Inflow Total -->
+                <p id="IFTNF"></p>
+            <!-- 昨日累積降雨量，Basin Rain Fall -->
+                <p id="BRFNF"></p>
+            <!-- 有效蓄水量，Effective Storage -->
+                <p id="ESNF"></p>
+            <!-- 有效庫容量，Effective Capacity -->
+                <p id="ECNF"></p>
+            </div>
+
+            <!-- 寶山水庫 -->
+                <div class="northFive">
+                <div class="nameFive">
+                    <h2>寶山水庫</h2>
+                </div>
+            <!-- 本日累積雨量，Inflow Total -->
+                <p id="IFTNFI"></p>
+            <!-- 昨日累積降雨量，Basin Rain Fall -->
+                <p id="BRFNFI"></p>
+            <!-- 有效蓄水量，Effective Storage -->
+                <p id="ESNFI"></p>
+            <!-- 有效庫容量，Effective Capacity -->
+                <p id="ECNFI"></p>
+            </div>
+
+            <!-- 寶山第二水庫 -->
+            <div class="northSix">
+                <div class="nameSix">
+                    <h2>寶山第二水庫</h2>
+                </div>
+            <!-- 本日累積雨量，Inflow Total -->
+                <p id="IFTNS"></p>
+            <!-- 昨日累積降雨量，Basin Rain Fall -->
+                <p id="BRFNS"></p>
+            <!-- 有效蓄水量，Effective Storage -->
+                <p id="ESNS"></p>
+            <!-- 有效庫容量，Effective Capacity -->
+                <p id="ECNS"></p>
+            </div>
+
         </div>
 <!-- 中區 -->
         <div class="middle">
-            <div  v-for="item in arrM" class="middleItem">
-            </div>
+
         </div>
 <!-- 南區-->
         <div class="south">
-            <div  v-for="item in arrS" class="southItem">
-            </div>
+
         </div>
     </div>
 </template>
@@ -91,41 +157,133 @@
 .content{
     width: 100vw;
     height: 100vh;
+
+    label{
+            font-size: 28pt;
+            margin-left: 11vmin;
+            color: aliceblue;
+        }
+        
 //北部區域
     .north{
         width: 90vw;
-        height: 60vh;
-        display: grid;
+        height: 104vh;
         margin: auto;
         border: 1px solid black;
+        display: flex;
+        gap: 3vmin;
+        flex-wrap: wrap;
 
-        label{
-            font-size: 28pt;
-            margin: 0;
-        }
-
+//石門水庫
         .northOne{
             width: 20vw;
             height: 50vh;
             background-color: aliceblue;
             border-radius: 5px;
-            border: 1px solid black;
-
             .nameOne{
                 width: 20vw;
-                height: 10vh;
+                height: 7vh;
                 background-color: lightslategray;
-                color: aliceblue;
-                
-
+                color: aliceblue;            
                 h2{
                     text-align: center;
                     margin: 0;
-                    padding-top: 2vmin;
+                    padding-top: 1vmin;
                 }
-
             }
-
+        }
+//西勢水庫
+        .northTwo{
+            width: 20vw;
+            height: 50vh;
+            background-color: aliceblue;
+            border-radius: 5px;
+            .nameTwo{
+                width: 20vw;
+                height: 7vh;
+                background-color: lightslategray;
+                color: aliceblue;
+                h2{
+                    text-align: center;
+                    margin: 0;
+                    padding-top: 1vmin;
+                }
+            }
+        }
+//新山水庫
+        .northThree{
+            width: 20vw;
+            height: 50vh;
+            background-color: aliceblue;
+            border-radius: 5px;
+            .nameThree{
+                width: 20vw;
+                height: 7vh;
+                background-color: lightslategray;
+                color: aliceblue;
+                h2{
+                    text-align: center;
+                    margin: 0;
+                    padding-top: 1vmin;
+                }
+            }
+        }
+//翡翠水庫
+        .northFour{
+            width: 20vw;
+            height: 50vh;
+            background-color: aliceblue;
+            border-radius: 5px;
+            .nameFour{
+                width: 20vw;
+                height: 7vh;
+                background-color: lightslategray;
+                color: aliceblue;
+                
+                h2{
+                    text-align: center;
+                    margin: 0;
+                    padding-top: 1vmin;
+                }
+            }
+        }
+//寶山水庫
+        .northFive{
+            width: 20vw;
+            height: 50vh;
+            background-color: aliceblue;
+            border-radius: 5px;
+            .nameFive{
+                width: 20vw;
+                height: 7vh;
+                background-color: lightslategray;
+                color: aliceblue;
+                
+                h2{
+                    text-align: center;
+                    margin: 0;
+                    padding-top: 1vmin;
+                }
+            }
+        }
+//寶山第二水庫
+        .northSix{
+            width: 20vw;
+            height: 50vh;
+            background-color: aliceblue;
+            border-radius: 5px;
+            .nameSix{
+                width: 20vw;
+                height: 7vh;
+                background-color: lightslategray;
+                color: aliceblue;
+                
+                h2{
+                    text-align: center;
+                    margin: 0;
+                    padding-top: 1vmin;
+                }
+            }
         }
     }
 
