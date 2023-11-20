56d795aaab981eb31f000118a76bb462f9513b0a
Jerrylac <j78779915@gmail.com>
2023-11-20 14:41:33 +0800

    SVGdata

--- src/main.js
+++ src/main.js
@@ -10,11 +10,15 @@ app.use(router)
 
 app.mount('#app')
 
+let obj1={}
+let obj2={}
+
 //水庫基本資料
 fetch("https://fhy.wra.gov.tw/WraApi/v1/Reservoir/Station")
 .then(response=>response.json())
 .then(data=>{
     console.log(data)
+
 })
 
 //水庫即時資料
@@ -22,6 +26,7 @@ fetch("https://fhy.wra.gov.tw/WraApi/v1/Reservoir/RealTimeInfo")
 .then(response=>response.json())
 .then(data=>{
     console.log(data)
+    obj1=data
 })
 
 //水庫每日營運狀況
@@ -29,12 +34,39 @@ fetch("https://data.wra.gov.tw/OpenAPI/api/OpenData/50C8256D-30C5-4B8D-9B84-2E14
 .then(response=>response.json())
 .then(data=>{
     console.log(data)
+    obj2=data
 })
 
 //水庫統計資料
 fetch("https://fhy.wra.gov.tw/WraApi/v1/Reservoir/Daily")
 .then(response=>response.json())
 .then(data=>{
-    console.log(data)
+    const content=document.getElementById("content")
+const reservoirOfS=document.querySelectorAll(".reservoirOfS")
+
+const rname= document.getElementById("rname")
+const Percentage=document.getElementById("Percentage")
+const btn=document.getElementById("btn")
+
+
+console.dir(reservoirOfS)
+
+reservoirOfS.forEach(reservoir=>{
+  reservoir.addEventListener("mouseover",function(){
+    obj2.responseData.forEach(item1=>{
+      if(reservoir.getAttribute("value")==item1.ReservoirName){
+        rname.innerText=item1.ReservoirName
+        // console.log(item1.ReservoirIdentifier); 
+        obj1.forEach(item2=>{
+          if(item1.ReservoirIdentifier==item2.StationNo){
+            Percentage.innerText=Math.floor(item2.PercentageOfStorage)+"% "
+          }
+        })
+        btn.innerHTML=' <a href="./Visual" style="text-decoration:none;"> 點我查看詳細</a>'
+      }
+    })
+    
+  })
+})
 })
 
