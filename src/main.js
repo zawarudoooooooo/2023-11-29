443ed6c3ea3994b738b005129a4a5e9d986c0df9
Roy <wave7410@gmail.com>
2023-11-22 16:56:18 +0800

    update

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ app.mount("#app");
 let obj1 = {};
 let obj2 = {};
 
-//水庫基本資料
+// //水庫基本資料
 // fetch("https://fhy.wra.gov.tw/WraApi/v1/Reservoir/Station")
 //   .then((response) => response.json())
 //   .then((data) => {
@@ -21,7 +21,7 @@ let obj2 = {};
 //     console.log(data);
 //   });
 
-//水庫即時資料
+// //水庫即時資料
 // fetch("https://fhy.wra.gov.tw/WraApi/v1/Reservoir/RealTimeInfo")
 //   .then((response) => response.json())
 //   .then((data) => {
@@ -29,7 +29,7 @@ let obj2 = {};
 //     obj1 = data;
 //   });
 
-//水庫每日營運狀況
+// //水庫每日營運狀況
 // fetch("https://data.wra.gov.tw/OpenAPI/api/OpenData/50C8256D-30C5-4B8D-9B84-2E14D5C6DF71/Data?size=1000&page=100")
 //   .then((response) => response.json())
 //   .then((data) => {
@@ -37,7 +37,7 @@ let obj2 = {};
 //     obj2 = data;
 //   });
 
-//水庫統計資料
+// //水庫統計資料
 // fetch("https://fhy.wra.gov.tw/WraApi/v1/Reservoir/Daily")
 //   .then((response) => response.json())
 //   .then((data) => {
